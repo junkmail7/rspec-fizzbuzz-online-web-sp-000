ba29f0ceeb53160776feeb50db07167e40462bd1
William Pannell <lpann5@gmail.com>
2020-10-30 00:37:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if (num % 3 == 0 && num % 5 == 0)
+    return "fizzbuzz"
   elsif num % 3 == 0
     return "fizz"
   elsif num % 5 == 0
