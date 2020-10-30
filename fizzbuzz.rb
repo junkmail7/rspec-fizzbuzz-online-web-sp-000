2f72ab16327901438901bb5b3050fcb7c4dffc66
William Pannell <lpann5@gmail.com>
2020-10-30 00:37:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num % 3 =
+  if (num % 3 == 0 && num % 5 == 0)
+  elsif num % 3 == 0
+    return "fizz"
+  elsif num % 5 == 0
+    return "buzz"
+  else
+    return nil
+  end
+    
+    
 end
